592b8dea596c711aafcaa45b9eefc4b504dda3cd
Ali Kemal Çelenk <alikemalcelenkk@gmail.com>
2021-03-09 16:22:43 +0300

    created layout

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+//components
+import Layout from '../components/layout';
+
 //components - styled-system
 import Box from '../components/styledSystem/box';
 
 function HomePage() {
-    return <Box>Welcome to Next.js!</Box>
+    return (
+        <Layout>
+            <Box>header</Box>
+            <Box>main</Box>
+            <Box>footer</Box>
+        </Layout>
+    )
 }
 
 export default HomePage
